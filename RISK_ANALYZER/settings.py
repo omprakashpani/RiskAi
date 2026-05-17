378913950a7a74d3d73beec83ef1a95480e88167
Om Prakash Pani <168963641+omprakashpani@users.noreply.github.com>
2026-05-17 22:21:31 +0530

    Update settings.py

--- RISK_ANALYZER/settings.py
+++ RISK_ANALYZER/settings.py
@@ -26,6 +26,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'risk_analyzer.urls'
